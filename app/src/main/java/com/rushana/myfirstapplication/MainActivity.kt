5de61592b3c8cf6ffb67d200236b4164603c9a39
Your Name <you@example.com>
2021-10-30 16:27:57 +0400

    alphabet in console

--- app/src/main/java/com/rushana/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/rushana/myfirstapplication/MainActivity.kt
@@ -2,10 +2,32 @@ package com.rushana.myfirstapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlin.collections.MutableSet as MutableSet1
+import kotlin.collections.indexOf as indexOf1
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val alphabet = listOf<String>("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
+        println(alphabet.filterIndexed { index, String -> index % 2 != 0 })
+
+
+        }
+
+
+
+
+
     }
-}
+
+
+
+
+
