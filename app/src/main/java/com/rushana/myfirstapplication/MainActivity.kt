92dd44cefc14351d279674c5d55c87d08e67453f
Your Name <you@example.com>
2021-11-04 18:24:31 +0400

    commit

--- app/src/main/java/com/rushana/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/rushana/myfirstapplication/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.rushana.myfirstapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import kotlin.collections.MutableSet as MutableSet1
-import kotlin.collections.indexOf as indexOf1
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+private const val Tag = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,13 +15,40 @@ class MainActivity : AppCompatActivity() {
         val alphabet = listOf<String>("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
         println(alphabet.filterIndexed { index, String -> index % 2 != 0 })
 
+        Log.d(Tag, "Унылая пора! Очей очарованье!")
 
         }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(Tag, "Приятна мне твоя прощальная краса —")
 
+    }
 
+    override fun onResume() {
+        super.onResume()
+        Log.d(Tag, "Люблю я пышное природы увяданье,")
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d (Tag, "И мглой волнистою покрыты небеса,")
+    }
 
+    override fun onPause() {
+        super.onPause()
+        Log.d(Tag, "В багрец и в золото одетые леса,")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(Tag, "В их сенях ветра шум и свежее дыханье,")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+    }
     }
 
 
